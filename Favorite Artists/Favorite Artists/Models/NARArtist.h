e39cb9ba8e265b474823161887ddeea36ae38569
Nicolas Rios <nickrios33@yahoo.com>
2020-05-31 19:50:23 -0700

    created models

---
+++ Favorite Artists/Favorite Artists/Models/NARArtist.h
@@ -0,0 +1,23 @@
+//
+//  NARArtist.h
+//  Favorite Artists
+//
+//  Created by Nicolas Rios on 5/31/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NARArtist : NSObject
+
+
+@property NSString *name;
+@property NSString *biography;
+@property int year;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/Models/NARArtist.m
@@ -0,0 +1,21 @@
+//
+//  NARArtist.m
+//  Favorite Artists
+//
+//  Created by Nicolas Rios on 5/31/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import "NARArtist.h"
+
+@implementation NARArtist
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"Name: %@, Year: %d", self.name, self.year];
+}
+
+
+
+
+
+@end
