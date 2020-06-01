855f72038d1e7c2feda18971582b74b683100239
Nicolas Rios <nickrios33@yahoo.com>
2020-05-31 18:33:50 -0700

    Created Xcode ProJect

---
+++ Favorite Artists/Favorite Artists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Favorite Artists
+//
+//  Created by Nicolas Rios on 5/31/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Favorite Artists/Favorite Artists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Favorite Artists
+//
+//  Created by Nicolas Rios on 5/31/20.
+//  Copyright © 2020 Nicolas Rios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
